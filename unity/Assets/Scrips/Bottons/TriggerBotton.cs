c34f632898db4f0b422f17880efc3fa36a78bbd5
Nerea E <nereaesp@ucm.es>
2024-02-20 18:32:52 +0100

    Create personal testscene and bottons scripts

---
+++ unity/Assets/Scrips/Bottons/TriggerBotton.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBotton : MonoBehaviour
+{
+    // Block with PublicVariables
+
+    // Block with [attributed]-variables (ex. SerializeField)
+
+    // Block with _privateVariables
+
+    // Block with references
+
+    // Block with MonoBehaviour life-cycle methods (ONLY mono-functions)
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    // Block with custom private methods
+
+    // Update methods
+    private void Update()
+    {
+        
+    }
+
+    // Block with custom public methods
+}
