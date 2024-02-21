4592c76ef44dab53e8c80f9b7e17579e0a9c1af4
Nerea E <nereaesp@ucm.es>
2024-02-21 20:14:25 +0100

    Create Ipickable

---
+++ EG6/Assets/Code/Scripts/PickUp/IPickable/IPickable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    // Interface for pickable objects
+    public interface IPickable
+    {
+        void PickUp();
+    }
+
