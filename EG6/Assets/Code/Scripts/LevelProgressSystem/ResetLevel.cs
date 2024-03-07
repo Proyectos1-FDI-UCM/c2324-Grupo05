856f00c70aa6bd28e2091fc8a3db03667deb4260
Andrea G.A <andrgo37@ucm.es>
2024-03-07 19:19:44 +0100

    Modified Exterior scene and water reset

---
+++ EG6/Assets/Code/Scripts/LevelProgressSystem/ResetLevel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<ChildMovement>() != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+    }
+}
