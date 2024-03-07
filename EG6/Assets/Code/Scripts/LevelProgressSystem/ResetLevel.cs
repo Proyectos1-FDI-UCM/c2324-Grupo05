47638adea169dce86e4f591813f6551f781b6d8f
Andrea G.A <andrgo37@ucm.es>
2024-03-07 20:02:36 +0100

    Fixed map

--- EG6/Assets/Code/Scripts/LevelProgressSystem/ResetLevel.cs
+++ EG6/Assets/Code/Scripts/LevelProgressSystem/ResetLevel.cs
@@ -5,14 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class ResetLevel : MonoBehaviour
 {
-    
-    
+    private LocalObjectHandler _localObjectHandler;
+
+
+    private void Start()
+    {
+        _localObjectHandler = FindObjectOfType<LocalObjectHandler>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<ChildMovement>() != null)
         {
+            _localObjectHandler.SaveLocalState();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
         }
     }
 }
+
