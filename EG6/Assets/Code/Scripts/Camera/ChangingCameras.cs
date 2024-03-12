2ce64aebb9a6864b65d54558a240a768f55ed3c6
Andrea G.A <andrgo37@ucm.es>
2024-03-12 21:11:19 +0100

    Update Camera follows active character

--- EG6/Assets/Code/Scripts/Camera/ChangingCameras.cs
+++ EG6/Assets/Code/Scripts/Camera/ChangingCameras.cs
@@ -7,18 +7,42 @@ public class ChangingCameras : MonoBehaviour
 {
     // Block with private (or protected) _fields
     [SerializeField] private GameObject _virtualCamera;
+    [SerializeField] private GameObject _target;
+    [SerializeField] private Transform _child;
+    [SerializeField] private Transform _penguin;
+    [SerializeField] private CharacterSwitcher _switcher;
+
 
     private void Start()
     {
         _virtualCamera.SetActive(false);
     }
 
+    private void Update()
+    {
+        //Check whether the child or the penguin is moving
+        //Camera should follow the one that is bein controlled at that moment
+
+        if (_switcher._isControllingChild)
+        {
+            _target.transform.position = _child.position;
+            Debug.Log("niño");
+        }
+        else
+        {
+            _target.transform.position = _penguin.position;
+            Debug.Log("pinguino");
+        }
+
+    }
+
 
     public virtual void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<ChildMovement>())
+        Debug.Log("hellou");
+        if (collision.gameObject.GetComponent<ChildMovement>() != null)
         {
-            Debug.Log("Entra");
+            
             _virtualCamera.SetActive(true);
             
         }
@@ -29,9 +53,9 @@ public class ChangingCameras : MonoBehaviour
     
     public virtual void OnTriggerExit2D(Collider2D other)
     {
-        if (other.GetComponent<ChildMovement>())
+        if (other.GetComponent<ChildMovement>() != null)
         {
-            Debug.Log("Sale");
+            
             _virtualCamera.SetActive(false);
 
         }
