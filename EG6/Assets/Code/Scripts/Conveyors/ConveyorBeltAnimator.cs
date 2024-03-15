2b099b0f406a36899a33210f2d52f3c6f5558726
Claudia <149375895+Clauudz@users.noreply.github.com>
2024-03-15 12:06:30 +0100

    Add conveyor belt animation

---
+++ EG6/Assets/Code/Scripts/Conveyors/ConveyorBeltAnimator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBeltAnimator : MonoBehaviour
+{
+    // Block with custom classes or structures
+
+    // Block with private (or protected) _fields
+
+    // Block with public Properties {get; set;}
+
+    // Block with MonoBehaviour life-cycle methods (ONLY mono-functions)
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    // MonoBehaviour update methods
+    private void Update()
+    {
+        
+    }
+
+    // Block with custom private Methods 
+
+    // Block with custom public Methods (with summary if it has complex logic)
+}
