74e9fcf8fad5f120532f1cfdf5cb6df5c27803aa
Diegoucm <dimart15@ucm.es>
2024-03-26 14:52:45 +0100

    Update IceSpawner now only spawns if there isnt colision with MovableObj

--- EG6/Assets/Code/Scripts/Non breakable ice/IceSpawner.cs
+++ EG6/Assets/Code/Scripts/Non breakable ice/IceSpawner.cs
@@ -13,28 +13,57 @@ public class IceSpawner : MonoBehaviour
 
     //Reference to transform
     private Transform _myTransform;
+    private BoxCollider2D _boxCollider2D;
+    private bool _spawnerBlocked;
 
     private void Start()
     {
         _myTransform = transform;
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+        _spawnerBlocked = false;
     }
 
     // MonoBehaviour update methods
     private void Update()
     {
         _currentTime += Time.deltaTime;
-        if(_currentTime >= _spawnTime)
+
+        // If the spawner is not blocked and the time to spawn has passed then spawn ice
+        if (_spawnerBlocked == false && _currentTime >= _spawnTime ) 
         {
             _currentTime = 0;
 
             //New ice
             SpawnIce();
+
         }
+        
     }
 
     private void SpawnIce()
     {
         Instantiate(_ice, _myTransform.position, Quaternion.identity);
     }
-    
+
+    // Detect if the spawner is blocked by an object with MovableObject component
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.GetComponent<MovableObject>() != null)
+        {
+            _spawnerBlocked = true;
+            Debug.Log(_spawnerBlocked);
+        }
+       
+    }
+
+    // Detect if the spawner is not blocked by an object with MovableObject component
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<MovableObject>() != null)
+        {
+            _spawnerBlocked = false;
+            Debug.Log(_spawnerBlocked);
+        }
+    }
+
 }
