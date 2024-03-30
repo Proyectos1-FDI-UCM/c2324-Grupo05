d103424f8b4762d8d28c49dfbeb1eb98f425dfa7
Nerea E <nereaesp@ucm.es>
2024-03-30 13:25:13 +0100

    ice despawner

---
+++ EG6/Assets/Code/Scripts/Non breakable ice/IceDespawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class IceDespawner : MonoBehaviour
+{
+    //[SerializeField] private GameObject _ice;
+  
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<MovableObject>()!= null)
+        {
+
+            Destroy(collision.gameObject);
+        
+        }
+    }
+}
