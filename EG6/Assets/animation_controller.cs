9ecef18197b08b5c6a775462967073877b8cf7be
ThatCreep2089 <alicia.pb547@gmail.com>
2024-04-03 11:00:22 +0200

    aaaa

--- EG6/Assets/animation_controller.cs
+++ EG6/Assets/animation_controller.cs
@@ -11,22 +11,33 @@ public class animation_controller : MonoBehaviour
     // Block with public Properties {get; set;}
 
     // Block with MonoBehaviour life-cycle methods (ONLY mono-functions)
+    public GameObject ChildMovement;
+    private ChildMovement _Child;
     private void Awake()
     {
+
         
     }
-
+    private Animation anim;
     private void Start()
     {
-        
+        anim = gameObject.GetComponent<Animation>();
+        _Child = gameObject.GetComponent<ChildMovement>();
     }
 
     // MonoBehaviour update methods
     private void Update()
     {
-        
-    }
+        if (anim.isPlaying)
+        {
+            return;
+        }
+        if (_Child.IsMoving & _Child.MovementDirection.x > 0 & _Child.MovementDirection.y > 0)
+        {
+            anim.Play("WFront_kid");
+        }
 
+    }
     // Block with custom private Methods 
 
     // Block with custom public Methods (with summary if it has complex logic)
