8904c3ac50e0c45961e3ebd680a659f097061b33
Nerea E <nereaesp@ucm.es>
2024-04-05 11:26:32 +0200

    map

--- EG6/Assets/Code/Scripts/PickUp/SpawnPiece.cs
+++ EG6/Assets/Code/Scripts/PickUp/SpawnPiece.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class SpawnPiece : MonoBehaviour
 {
     [SerializeField] GameObject _piece;
+    private int _pieces;
     private void Awake()
     {
         _piece.SetActive(false);
+        _pieces = PlayerPrefs.GetInt("pieza");
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<ChildMovement>() != null)
-        { 
-        
-            _piece.SetActive(true);
+        {
+            if (_pieces==0)
+            {
+                _piece.SetActive(true);
+
+            }
+            
         
         }
     }
