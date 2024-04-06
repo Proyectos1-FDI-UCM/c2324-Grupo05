d11840c000807e39d3ce1be1ea2c9084ef980d9d
Dekros <clearsky3210@gmail.com>
2024-04-06 20:53:44 +0200

    Add Water

--- EG6/Assets/Code/Scripts/LevelProgressSystem/LevelResetter.cs
+++ EG6/Assets/Code/Scripts/LevelProgressSystem/LevelResetter.cs
@@ -7,9 +7,9 @@ using UnityEngine.SceneManagement;
 /// This class is used to reset the level when the player crosses the reset trigger 
 /// (like water or something else that kills the player)
 /// </summary>
-public class LevelResetter : MonoBehaviour
+public abstract class LevelResetter : MonoBehaviour
 {
-    private LocalObjectHandler _localObjectHandler;
+    protected LocalObjectHandler _localObjectHandler;
 
 
     private void Start()
@@ -17,29 +17,13 @@ public class LevelResetter : MonoBehaviour
         _localObjectHandler = FindObjectOfType<LocalObjectHandler>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.GetComponent<ChildMovement>() != null)
+        if (collision.gameObject.GetComponent<ChildMovement>() != null || collision.gameObject.GetComponent<PenguinMovement>() != null)
         {
             Debug.Log("Resetting level");
             _localObjectHandler.SaveLocalState();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-        else if (collision.gameObject.GetComponent<PenguinMovement>() != null)
-        {
-            PenguinMovement penguinMovement = collision.gameObject.GetComponent<PenguinMovement>();
-            penguinMovement.MovementMode = MovementMode.Swimming;
-            penguinMovement.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision) 
-    {
-        if (collision.gameObject.GetComponent<PenguinMovement>() != null)
-        {
-            PenguinMovement penguinMovement = collision.gameObject.GetComponent<PenguinMovement>();
-            penguinMovement.MovementMode = MovementMode.Walking;
-            penguinMovement.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         }
     }
 }
---
+++ EG6/Assets/Code/Scripts/LevelProgressSystem/Water.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+    public class Water : LevelResetter
+{
+    
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<ChildMovement>() != null)
+        {
+            Debug.Log("Resetting level");
+            _localObjectHandler.SaveLocalState();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else if (collision.gameObject.GetComponent<PenguinMovement>() != null)
+        {
+            PenguinMovement penguinMovement = collision.gameObject.GetComponent<PenguinMovement>();
+            penguinMovement.MovementMode = MovementMode.Swimming;
+            penguinMovement.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) 
+    {
+        if (collision.gameObject.GetComponent<PenguinMovement>() != null)
+        {
+            PenguinMovement penguinMovement = collision.gameObject.GetComponent<PenguinMovement>();
+            penguinMovement.MovementMode = MovementMode.Walking;
+            penguinMovement.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+}
