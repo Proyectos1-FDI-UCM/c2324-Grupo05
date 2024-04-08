37406444d4dace967692914696dd04fc68208e44
Diegoucm <dimart15@ucm.es>
2024-04-08 18:43:02 +0200

    Update piece respawn when falling to lava

--- EG6/Assets/Code/Scripts/PickUp/PieceRespawnAndPlayer.cs
+++ EG6/Assets/Code/Scripts/PickUp/PieceRespawnAndPlayer.cs
@@ -10,11 +10,12 @@ public class PieceRespawnAndPlayer : MonoBehaviour
     [SerializeField] GameObject _tppieza;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.GetComponent<MovableObject>() != null)
+        if (collision.gameObject.GetComponent<Piece>() != null)
         {
 
             _player.transform.position = _tpplayer.transform.position;
             _pieza.transform.position = _tppieza.transform.position;
+            _pieza.GetComponent<MovableObject>().enabled = false; // Disable the MovableObject component, there is an error here and it is that the movable object cant be enabled again
 
 
 
