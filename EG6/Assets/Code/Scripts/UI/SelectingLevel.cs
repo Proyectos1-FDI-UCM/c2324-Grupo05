be46a814a12f9383ba3f9a43e94dd6b179b420cf
Diegoucm <dimart15@ucm.es>
2024-04-12 12:41:25 +0200

    Update select menu locked levels in red

--- EG6/Assets/Code/Scripts/UI/SelectingLevel.cs
+++ EG6/Assets/Code/Scripts/UI/SelectingLevel.cs
@@ -1,45 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro.Examples;
-using UnityEditor.UI;
 using UnityEngine;
-using UnityEngine.InputSystem.Controls;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 using LevelState = GlobalObjectRegistry.LevelState;
 
 public class SelectingLevelButton : MonoBehaviour
 {
-    [SerializeField] private int _id;
+    
     private GlobalObjectRegistry _globalObjectRegistry;
-    private UnityEngine.UI.Button _button ;
     private LevelState _levelState ;
+
+    private void Awake()
+    {
+        _globalObjectRegistry = GlobalObjectRegistry.instance;
+
+    }
 
     private void Start()
     {
         gameObject.SetActive(true);
         _levelState = _globalObjectRegistry.GetLevelState("Map-Exterior");
-        _globalObjectRegistry = GlobalObjectRegistry.instance;
-        _button = GetComponent<UnityEngine.UI.Button>();
-        if (_id > _levelState.LastCheckpointID)
-        {
-            _button.interactable = false;
-        }
-        
     }
 
-    public void LoadLevel()
+    public void LoadLevel(int indexLevel)
     {
-        
-
-        if (_id > _levelState.LastCheckpointID)
+        if (indexLevel > _levelState.LastCheckpointID)
         {
             Debug.Log("You can't load this level yet");
             return;
         }
 
-        _levelState.CurrentCheckpointID = _id;
+        _levelState.CurrentCheckpointID = indexLevel;
         _globalObjectRegistry.SaveLevelState(_levelState.PickedObjects, _levelState.OpenedDoors, _levelState.DestroyedObjects, _levelState.PressedButtons, _levelState.CurrentCheckpointID, "Map-Exterior");
         SceneManager.LoadScene("Map-Exterior");
     }
