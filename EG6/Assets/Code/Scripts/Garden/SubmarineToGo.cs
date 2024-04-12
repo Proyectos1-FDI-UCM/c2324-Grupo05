73de1b53e33ed02859aaf06581c06764953353c6
Nerea E. <nereaesp@ucm.es>
2024-04-12 13:04:24 +0200

    slight change garden

--- EG6/Assets/Code/Scripts/Garden/SubmarineToGo.cs
+++ EG6/Assets/Code/Scripts/Garden/SubmarineToGo.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class SubmarineToGo : MonoBehaviour
 {
     [SerializeField] GameObject _submarine;
+    [SerializeField] GameObject _eggtp;
+    [SerializeField] GameObject _egg;
     private void Start()
     {
         _submarine.SetActive(false);
@@ -15,6 +17,7 @@ public class SubmarineToGo : MonoBehaviour
         if (collision.gameObject.GetComponent<ChildMovement>() != null && collision.gameObject.GetComponent<PenguinMovement>() != null)
         {
 
+            _egg.transform.position = _eggtp.transform.position;
             _submarine.SetActive(true);
 
         }
