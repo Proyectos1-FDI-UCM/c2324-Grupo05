4a4dbb3c682f09d619c302a3a22a61832d412534
Nerea E. <nereaesp@ucm.es>
2024-04-16 17:51:56 +0200

    text in bedrrom works

--- EG6/Assets/Code/Scripts/PickPenguin.cs
+++ EG6/Assets/Code/Scripts/PickPenguin.cs
@@ -7,6 +7,17 @@ public class PickPenguin : MonoBehaviour
     [SerializeField] GameObject _egg;
     [SerializeField] GameObject _pannelEgg;
     [SerializeField] GameObject _collider;
+    private void Start()
+    {
+        _egg.SetActive(false);
+    }
+
+    public void PutEgg()
+    {
+
+        _egg.SetActive(true);
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<ChildMovement>() != null && GlobalObjectRegistry.instance.isEggPicked && GlobalObjectRegistry.instance.collectedPieces == 1)
--- EG6/Assets/Code/Scripts/PutEgg.cs
+++ EG6/Assets/Code/Scripts/PutEgg.cs
@@ -5,18 +5,15 @@ using UnityEngine;
 
 public class PutEgg : MonoBehaviour
 {
-    [SerializeField] GameObject _egg2;
-    private void Start()
-    {
-        _egg2.SetActive(false);
-    }
+    [SerializeField]private PickPenguin _pickpen;
+    
     private void OnTriggerEnter2D()
     {
         if (GlobalObjectRegistry.instance.collectedPieces == 1)
-        { 
-        
-            _egg2.SetActive(true);
-        
+        {
+
+            _pickpen.PutEgg();
+            Destroy(gameObject);
         }
     
     
