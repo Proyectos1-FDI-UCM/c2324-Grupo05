3097c08ef5f6bb590824e70803aaecaaf9aac06a
Dekros <clearsky3210@gmail.com>
2024-04-17 09:31:34 +0200

    Update EvolutionSpriteController

--- EG6/Assets/Code/Scripts/EvolutionSpriteController.cs
+++ EG6/Assets/Code/Scripts/EvolutionSpriteController.cs
@@ -1,36 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class is responsible for changing the sprite of the penguin based on how many pieces the player has collected.
+/// </summary>
 public class EvolutionSpriteController : MonoBehaviour
 {
     [SerializeField] private Sprite[] _evolutionSprite = new Sprite[4];
     //[SerializeField] private Animator[] _evolutionAnimation = new Animator[3];
 
     private SpriteRenderer _spriteRenderer;
-    //private Animator _penguinAnimator;
-
-
+    //private Animator _penguinAnimator
    
-
     private void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = _evolutionSprite[GlobalObjectRegistry.instance.collectedPieces];
         //_penguinAnimator = GetComponent<Animator>();
     }
 
     // MonoBehaviour update methods
     private void Update()
     {
-        //penguin shows the sprite and animator based on how many pieces the player has collected
-        _spriteRenderer.sprite = _evolutionSprite[GlobalObjectRegistry.instance.collectedPieces];
         //_penguinAnimator = _evolutionAnimation[GlobalObjectRegistry.instance.collectedPieces];
-
-
-
     }
-
-    // Block with custom private Methods 
-
-    // Block with custom public Methods (with summary if it has complex logic)
 }
