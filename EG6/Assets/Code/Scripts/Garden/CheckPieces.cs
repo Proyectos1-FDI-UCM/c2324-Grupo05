9c7fd5b1db61426ccd0052191617f7128bdc83e0
Nerea E. <nereaesp@ucm.es>
2024-04-17 20:47:39 +0200

    update garden

---
+++ EG6/Assets/Code/Scripts/Garden/CheckPieces.cs
@@ -0,0 +1,30 @@
+
+   using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckAllPieces : MonoBehaviour
+{
+    [SerializeField] GameObject _bottonExit;
+
+    [SerializeField] int _numpieces = 2; //3 for endgame, 2 test
+    private void Start()
+    {
+
+        _bottonExit.SetActive(false);
+    }
+
+    public void AllCollectedPieces()
+    {
+        if (GlobalObjectRegistry.instance.collectedPieces == _numpieces)
+        {
+
+            _bottonExit.SetActive(true);
+
+        }
+
+
+    }
+
+}
+
