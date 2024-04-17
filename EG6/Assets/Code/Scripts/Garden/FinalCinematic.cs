46a177143cbeb24b97091098c2adecf093fe8d0b
Nerea E. <nereaesp@ucm.es>
2024-04-17 09:56:57 +0200

    clean bedroom

--- EG6/Assets/Code/Scripts/Garden/FinalCinematic.cs
+++ EG6/Assets/Code/Scripts/Garden/FinalCinematic.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class FinalCinematic : MonoBehaviour
 {
-    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<ChildMovement>() != null || collision.gameObject.GetComponent<PenguinMovement>() != null)
--- EG6/Assets/Code/Scripts/ShowPenguinPannel.cs
+++ EG6/Assets/Code/Scripts/ShowPenguinPannel.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class ShowPenguinPannel : MonoBehaviour
 {
-  
     [SerializeField] GameObject _eggCanHelpPannel;
     [SerializeField] GameObject _collider;
-    [SerializeField] int _pieces=0;
+    [SerializeField] int _waitTime=8;
     private void Start()
     {
-      
+        
         _eggCanHelpPannel.SetActive(false);
         _collider.SetActive(true);
     }
-    private void OnTriggerExit2D(Collider2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.GetComponent<ChildMovement>() != null && GlobalObjectRegistry.instance.isEggPicked&& GlobalObjectRegistry.instance.collectedPieces==_pieces)
+
+        if (collision.gameObject.GetComponent<ChildMovement>() != null&& GlobalObjectRegistry.instance.isEggPicked)
         {
-          
+
             _collider.AddComponent<BoxCollider2D>();
             _eggCanHelpPannel.SetActive(true);
             Destroy(gameObject);
         }
+
+
     }
+    
+
+    
+
 
 }
