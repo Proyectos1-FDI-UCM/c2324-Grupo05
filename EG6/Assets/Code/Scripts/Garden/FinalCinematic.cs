d969aed6f8ef05edcb433b9d3eb37d0b7cf5d4f1
Dekros <clearsky3210@gmail.com>
2024-04-26 12:20:29 +0200

    Add GardenIce (2/2)

--- EG6/Assets/Code/Scripts/Garden/FinalCinematic.cs
+++ EG6/Assets/Code/Scripts/Garden/FinalCinematic.cs
@@ -1,17 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class FinalCinematic : MonoBehaviour
 {
     [SerializeField] GameObject _pannel;
     [SerializeField] private GameObject _finalTimeline;
     [SerializeField] private GameObject _credits;
+    [SerializeField] private UnityEngine.UI.Button _yesButton;
 
     private void Start()
     {
         _finalTimeline.SetActive(false);
+        if (Input.GetJoystickNames().Length != 0)
+        {
+            EventSystem.current.SetSelectedGameObject(_yesButton.gameObject);
+        }
     }
 
     public void OnClickYes()
