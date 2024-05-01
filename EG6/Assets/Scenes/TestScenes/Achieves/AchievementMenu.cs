d587d9f0dff091701fdfde4adbaeeabad19ce40e
Andrea G.A. <andrgo37@ucm.es>
2024-05-01 18:59:32 +0200

    achievement menu logic seams to work

--- EG6/Assets/Scenes/TestScenes/Achieves/AchievementMenu.cs
+++ EG6/Assets/Scenes/TestScenes/Achieves/AchievementMenu.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class AchievementMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _achieveBadge;
-    [SerializeField] private GameObject _achievement;
+    [SerializeField] private TextMeshProUGUI _achievementText;
     [SerializeField] private GameObject _buttonBackSelect;
+    public List<Achievement> Achievements;
+    private AchievementCollector _achievementCollector;
 
     //not available text
     [SerializeField] private GameObject _notAvailable;
@@ -15,31 +19,46 @@ public class AchievementMenu : MonoBehaviour
 
     private void Start()
     {
+        _achievementCollector = GetComponent<AchievementCollector>();
         _buttonBackSelect.SetActive(false);
         _notAvailable.SetActive(false);
         _achieveBadge.SetActive(true);
-        _achievement.SetActive(false);
+        _achievementText.enabled = false;
     }
 
     public void BackToSelect()
     {
         _achieveBadge.SetActive(true);
-        _achievement.SetActive(false);
+        _achievementText.enabled = false;
         _buttonBackSelect.SetActive(false);
     }
 
-    public void ShowAchievement()
+    public void ShowAchievement(string achievementName)
     {
-        //if achievement is unlocked, show achievement text
-        _achieveBadge.SetActive(false);
-        _achievement.SetActive(true);
-        _buttonBackSelect.SetActive(true);
         
-        //if achievement is locked
-        /*
-        _notAvailable.SetActive(true);
-        StartCoroutine(Waittext());
-        */
+        foreach (Achievement achievement in _achievementCollector.Achievements)
+        {
+            if (achievement.Name == achievementName)
+            {
+                //if achievement is unlocked, show achievement text
+                if (achievement.IsUnlocked == true)
+                {
+                    _achieveBadge.SetActive(false);
+                    _achievementText.text = achievement.Name;
+                    _achievementText.enabled = true;
+                    _buttonBackSelect.SetActive(true);
+                }
+                else
+                {
+                    //if achievement is locked
+                    _notAvailable.SetActive(true);
+                    StartCoroutine(Waittext());
+                    
+                }
+            }
+        }
+
+        
     }
 
     private IEnumerator Waittext()
