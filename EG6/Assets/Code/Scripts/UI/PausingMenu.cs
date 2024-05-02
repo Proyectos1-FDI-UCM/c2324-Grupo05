e2344d6fc629807b31aed21d5ecb61303dfbfee9
Andrea G.A. <andrgo37@ucm.es>
2024-05-02 10:10:46 +0200

    Basic layout settings menu

--- EG6/Assets/Code/Scripts/UI/PausingMenu.cs
+++ EG6/Assets/Code/Scripts/UI/PausingMenu.cs
@@ -1,13 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class PausingMenu : MonoBehaviour
 {
+    [SerializeField] GameObject _mainButtons;
+    [SerializeField] private GameObject _buttonBackSelect;
+    [SerializeField] GameObject _settings;
     private SavesManager _savesManager;
 
     private void Start()
     {
         _savesManager = FindObjectOfType<SavesManager>();
+        _mainButtons.SetActive(true);
+        _buttonBackSelect.SetActive(false);
+        _settings.SetActive(false);
         gameObject.SetActive(false);
         Time.timeScale = 1f;
     }
@@ -17,7 +25,14 @@ public class PausingMenu : MonoBehaviour
         gameObject.SetActive(true);
         Time.timeScale = 0f;
     }
-    
+
+    public void BackToSelect()
+    {
+        _mainButtons.SetActive(true);
+        _buttonBackSelect.SetActive(false);
+        _settings.SetActive(false);
+    }
+
     public void Resume()
     {
         Time.timeScale = 1f;
@@ -40,5 +55,12 @@ public class PausingMenu : MonoBehaviour
     {
         _savesManager.SaveGame();
         Application.Quit();
+    }
+
+    public void Settings()
+    {
+        _mainButtons.SetActive(false);
+        _buttonBackSelect.SetActive(true);
+        _settings.SetActive(true);
     }
 }
