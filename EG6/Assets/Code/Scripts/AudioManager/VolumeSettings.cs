1d67f3195af20626c1eacc05a92a7740edc9fbba
Andrea G.A. <andrgo37@ucm.es>
2024-05-02 10:38:10 +0200

    volume settings saves volume level

--- EG6/Assets/Code/Scripts/AudioManager/VolumeSettings.cs
+++ EG6/Assets/Code/Scripts/AudioManager/VolumeSettings.cs
@@ -9,9 +9,29 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private AudioMixer _audioMixer;
     [SerializeField] private Slider _audioSlider;
 
+    private void Start()
+    {
+        if(PlayerPrefs.HasKey("MasterVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+        }
+        
+    }
+
     public void SetMusicVolume()
     {
         float volume = _audioSlider.value;
         _audioMixer.SetFloat("Master", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("MasterVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        _audioSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+        SetMusicVolume();
     }
 }
