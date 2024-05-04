4fe6606ab93ad6f2da37219b3206342f752d5cd5
Andrea G.A. <andrgo37@ucm.es>
2024-05-04 18:54:44 +0200

    Trash collector slider and some fixes

--- EG6/Assets/Code/Scripts/UI/StadisticsMenu.cs
+++ EG6/Assets/Code/Scripts/UI/StadisticsMenu.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StadisticsMenu : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer[] _spriteRenderers;
-
+    [SerializeField] private int _nMaxTrash;
+    public Slider slider;
 
     private void Start()
     {
@@ -34,25 +37,29 @@ public class StadisticsMenu : MonoBehaviour
         if (GlobalObjectRegistry.instance.collectedPieces >= 2)
         {
             _spriteRenderers[2].color = Color.white;
+            _spriteRenderers[3].color = Color.white;
         }
         else
         {
             _spriteRenderers[2].color = Color.black;
+            _spriteRenderers[3].color = Color.black;
         }
 
         //3rd piece
         if (GlobalObjectRegistry.instance.collectedPieces >= 3)
         {
-            _spriteRenderers[3].color = Color.white;
+            _spriteRenderers[4].color = Color.white;
         }
         else
         {
-            _spriteRenderers[3].color = Color.black;
+            _spriteRenderers[4].color = Color.black;
         }
 
-
-
+        slider.maxValue = _nMaxTrash;
+        slider.value = GlobalObjectRegistry.instance.collectedTrash;
+        
     }
 
+    
     
 }
